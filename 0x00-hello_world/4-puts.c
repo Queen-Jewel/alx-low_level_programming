a65c887086b8ba1173706c63846a59a279749354
Jewel Jeff-Onyegbule <ketachi.jeff@gmail.com>
2022-09-09 16:58:53 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print thr string in the put function
+ *
+ * Description: using the main function
+ *
+ * this program prints "Programming is like building a multilingual puzzle
+ * Return: 0
+ */
+
+int main(void)
+{
+	putd("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
