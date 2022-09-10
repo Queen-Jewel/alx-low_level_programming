08d0906fc4d1732b89e391c85ab4584f4c291eae
Jewel Jeff-Onyegbule <ketachi.jeff@gmail.com>
2022-09-09 17:53:30 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,6 +17,6 @@ int main(void)
 	printf("size of a char: %ld byte(s)\n", sizeof(c));
 	printf("size of an int: %ld byte(s)\n", sizeof(a));
 	printf("size of a long int: %ld byte(s)\n", sizeof(li));
-	printf("size of a long long int: %ld byte(s)\n", sizeof(lli);
+	printf("size of a long long int: %ld byte(s)\n", sizeof(lli));
 	printf("sizr of a float: %ld byte(s)\n", sizeof(f));
 	return (0);
