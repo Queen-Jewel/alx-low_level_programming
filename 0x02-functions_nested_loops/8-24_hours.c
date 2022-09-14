ca7cbb0ad5b5a9b375101c6522f762ce2b332bc9
Jewel Jeff-Onyegbule <ketachi.jeff@gmail.com>
2022-09-14 03:37:27 -0700

    Task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * jack_bauer - function that prints every minute of the day of Jack Bauer.
+ * Return: 0
+ */
+
+void jack_bauer(void)
+{
+	int hours = 0;
+	int minutes = 0;
+	int hours_reminder;
+	int mins_reminder;
+
+	while (hours <= 23)
+	{
+		while (minutes <= 59)
+		{
+			mins_reminder = minutes % 10  hours_reminder = hours % 10;
+			_putchar(hours / 10 + '0');
+			_putchar(hours_reminder + '0')
+			_putchar(':');
+			_putchar(minutes / 10 + '0');
+			_putchar(minutes_reminder + '0')
+				minutes++;
+			_putchar('\n');
+		}
+		hours++;
+		minutes = 0;
+	}
+}
+
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -4,4 +4,5 @@ print c if it is a letter, upper or lowercase
 print the sign of a number
 a function that computes the absolute value of an integer
 a function that prints the last digit of a number.
+a function that prints every minute of the day 
 
