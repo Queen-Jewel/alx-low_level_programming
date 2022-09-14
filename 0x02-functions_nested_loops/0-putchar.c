d9897e0ec017c5d63329e7f84eabc6b39008f255
Jewel Jeff-Onyegbule <ketachi.jeff@gmail.com>
2022-09-13 18:39:09 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,19 +3,19 @@
 /**
  * main - print putchar
  *
- * Return: Always 0
+ * Return: Always 0 (Success)
  */
-void _putchar(char* name)
 int main(void)
 {
-	int i = 0;
-	char p[] = "_putchar"
-
-		for (i = 0; i < 8; i++)
-		{
-			_putchar(p[i]);
-			i++;
-		}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
