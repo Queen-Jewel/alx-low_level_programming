b363f0a2304c693fa5b3f6e657a646a61d6d9c57
Jewel Jeff-Onyegbule <ketachi.jeff@gmail.com>
2022-09-14 05:26:29 -0700

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -20,10 +20,14 @@ void times_table(void)
 			prod = num * mult;
 			
 			if (prod <= 9)
+			{
 				_putchar(' ');
+			}
 			else
+			{
 				_putchar((prod / 10) + '0');
 				_putchar((prod % 10) + '0');
+			}
 		}
 		_putchar('\n');
 	}
