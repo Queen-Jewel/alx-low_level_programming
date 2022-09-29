0b87d58a41efca04f41b87c599152ea9f700cdaa
Jewel Jeff-Onyegbule <ketachi.jeff@gmail.com>
2022-09-28 17:55:24 -0700

    Task 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- *  _pow_recursion - returns the value of x raised to the power of y
- *  @x: number to be raised
- *  @y: powerReturn: int of x to the power of y
+ * _pow_recursion - returns the value of x raised to the power of y
+ * @x: number to be raised
+ * @y: powerReturn: int of x to the power of y
+ * Return: int of x to the power of y
  */
 
 int _pow_recursion(int x, int y)
