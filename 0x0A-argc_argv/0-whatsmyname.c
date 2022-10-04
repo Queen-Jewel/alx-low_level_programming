4627f6734786460a9cacdce87c69f995539b7321
Jewel Jeff-Onyegbule <ketachi.jeff@gmail.com>
2022-10-04 13:46:24 -0700

    Task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,12 +4,13 @@
  * main - prints name of the program
  * @argc: argument count
  * @argv: argument vector
- * Return: this return to 0
+ * Return: int
  */
 
 int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[0]);
 	(void)argc;
+	printf("%s\n", argv[0]);
 	return (0);
+
 }
